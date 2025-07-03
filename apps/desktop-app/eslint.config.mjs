5ecdcaed302747bf9517903ea6d5d68f43d22059
NoNamer777 <oscar.wellner@gmail.com>
2025-07-03 17:10:24 +0200

    Allow empty functions

--- apps/desktop-app/eslint.config.mjs
+++ apps/desktop-app/eslint.config.mjs
@@ -1,3 +1,11 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [...baseConfig];
+export default [
+    ...baseConfig,
+    {
+        files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+        rules: {
+            '@typescript-eslint/no-empty-function': 'off',
+        },
+    },
+];
