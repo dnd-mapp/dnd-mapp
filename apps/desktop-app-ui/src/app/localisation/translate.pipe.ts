68d668cf1a23585623bbd1a325df369f7507449f
NoNamer777 <oscar.wellner@gmail.com>
2025-07-04 22:33:22 +0200

    Add TranslatePipe to display translations in the UI

---
+++ apps/desktop-app-ui/src/app/localisation/translate.pipe.ts
@@ -0,0 +1,15 @@
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { TranslationKey } from '@dnd-mapp/desktop-shared';
+import { TranslationService } from './translation.service';
+
+@Pipe({
+    name: 'translate',
+    pure: false,
+})
+export class TranslatePipe implements PipeTransform {
+    private readonly translationService = inject(TranslationService);
+
+    public transform(translationKey: TranslationKey) {
+        return this.translationService.getTranslation(translationKey)();
+    }
+}
