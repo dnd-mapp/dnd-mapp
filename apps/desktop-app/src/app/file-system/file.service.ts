48d705da86b4be719aa9ef278436f682ce58c5c9
NoNamer777 <oscar.wellner@gmail.com>
2025-07-05 12:27:22 +0200

    Add logging to the FileService

--- apps/desktop-app/src/app/file-system/file.service.ts
+++ apps/desktop-app/src/app/file-system/file.service.ts
@@ -1,5 +1,6 @@
 import { tryCatch } from '@dnd-mapp/shared';
 import { mkdir, readFile, stat, writeFile } from 'fs/promises';
+import { LogService } from '../logging';
 
 const JSON_STRINGIFY_SPACER = 4;
 
@@ -12,43 +13,54 @@ export class FileService {
     }
     private static _instance: FileService;
 
+    private logService = LogService.withContext(FileService.name);
+
     public async doesPathExists(path: string) {
+        await this.logService.debug(`Validating existence of path "${path}"`);
         const { error: statError } = await tryCatch(stat(path));
 
         if (statError) {
-            if ('code' in statError && statError.code === 'ENOENT') return false;
-            console.error(statError);
+            if ('code' in statError && statError.code === 'ENOENT') {
+                await this.logService.warn(`Path does not exist`);
+                return false;
+            }
+            await this.logService.error(`Something unexpected went wrong while get path stats`, statError);
         }
         return true;
     }
 
     public async createFolder(folderPath: string) {
-        if (await this.doesPathExists(folderPath)) return;
-
+        await this.logService.debug(`Creating folder "${folderPath}"`);
+        if (await this.doesPathExists(folderPath)) {
+            await this.logService.debug(`Folder already exists`);
+            return;
+        }
         const { error: createFolderError } = await tryCatch(mkdir(folderPath, { recursive: true }));
 
         if (createFolderError) {
-            console.error(createFolderError);
+            await this.logService.warn(`Something went wrong while creating folder "${folderPath}"`, createFolderError);
         }
     }
 
     public async readFile<T>(filePath: string) {
+        await this.logService.debug(`Reading file on path "${filePath}"`);
         if (!(await this.doesPathExists(filePath))) return null;
         const { data: fileContents, error: readError } = await tryCatch(readFile(filePath, { encoding: 'utf8' }));
 
         if (readError) {
-            console.error(readError);
+            await this.logService.warn(`Something went wrong while reading file on path "${filePath}"`, readError);
         }
         return JSON.parse(fileContents) as T;
     }
 
     public async writeFile<T>(filePath: string, data: T) {
+        await this.logService.debug(`Writing file on path "${filePath}"`);
         const fileContents = JSON.stringify(data, null, JSON_STRINGIFY_SPACER);
 
         const { error: writeError } = await tryCatch(writeFile(filePath, `${fileContents}\n`));
 
         if (writeError) {
-            console.error(writeError);
+            await this.logService.warn(`Something went wrong while writing file on path "${filePath}"`, writeError);
         }
     }
 }
