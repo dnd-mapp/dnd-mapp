0e31ff5fddfc5067b2f6b31db750f69dd2dd4285
NoNamer777 <oscar.wellner@gmail.com>
2025-07-05 16:58:26 +0200

    Add logLevel into the AppConfig

--- apps/desktop-app/src/app/config/models.ts
+++ apps/desktop-app/src/app/config/models.ts
@@ -1,4 +1,11 @@
-import { DEFAULT_LOCALE, Locale, Locales } from '@dnd-mapp/desktop-shared';
+import {
+    DEFAULT_LOCALE,
+    DEFAULT_LOG_LEVEL,
+    Locale,
+    Locales,
+    SeverityLevel,
+    SeverityLevels,
+} from '@dnd-mapp/desktop-shared';
 import { plainToInstance } from 'class-transformer';
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
@@ -11,6 +18,11 @@ export class AppConfig {
     @IsNotEmpty({ message: 'Locale should not be an empty string.' })
     @IsString({ message: 'Locale should be a string with a valid supported Locale.' })
     public locale: Locale;
+
+    @IsEnum(SeverityLevels, { message: ({ value }) => `"${value}" is not a valid supported LogLevel.` })
+    @IsNotEmpty({ message: 'logLevel should not be an empty string.' })
+    @IsString({ message: 'logLevel should be a string with a valid supported LogLevel.' })
+    public logLevel: SeverityLevel;
 }
 
 export type AppSetting = keyof AppConfig;
@@ -19,4 +31,5 @@ export type AppSettingType<Setting extends AppSetting> = AppConfig[Setting];
 
 export const DEFAULT_APP_CONFIG = plainToInstance(AppConfig, {
     locale: DEFAULT_LOCALE,
-});
+    logLevel: DEFAULT_LOG_LEVEL,
+} satisfies AppConfig);
