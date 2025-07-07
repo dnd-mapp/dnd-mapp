e98ffaa36ce52e41ebadaef251eada1d23fd0037
NoNamer777 <oscar.wellner@gmail.com>
2025-07-07 21:35:11 +0200

    Add client app for the Authorization Server

---
+++ apps/auth-server-client/karma.conf.js
@@ -0,0 +1,41 @@
+const { join } = require('path');
+
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/6.4/config/configuration-file.html
+module.exports = (config) => {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-jasmine-html-reporter', 'karma-coverage'],
+        client: {
+            useIframe: false,
+            jasmine: {
+                // you can add configuration options for Jasmine here
+                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+                // for example, you can disable the random execution with `random: false`
+                // or set a specific seed with `seed: 4321`
+                random: true,
+            },
+        },
+        jasmineHtmlReporter: {
+            suppressAll: true,
+        },
+        coverageReporter: {
+            dir: join(__dirname, '../../reports/apps/auth-server-client'),
+            // check: {
+            //     branches: 80,
+            //     functions: 80,
+            //     lines: 80,
+            //     statements: 80,
+            //     excludes: [],
+            // },
+            subdir: '.',
+            reporters: [{ type: 'html' }, { type: 'text-summary' }],
+        },
+        reporters: ['dots', 'kjhtml'],
+        browsers: ['ChromeHeadless'],
+        restartOnFileChange: true,
+        failOnEmptyTestSuite: false,
+        reportSlowerThan: 300,
+    });
+};
---
+++ apps/auth-server-client/src/app/core/root/root.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component, provideZonelessChangeDetection } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { RootHarness } from '@dnd-mapp/auth-server-client/testing';
+import { RootComponent } from './root.component';
+
+describe('RootComponent', () => {
+    @Component({
+        template: `<dma-root />`,
+        imports: [RootComponent],
+    })
+    class TestComponent {}
+
+    async function setupTest() {
+        TestBed.configureTestingModule({
+            imports: [TestComponent],
+            providers: [provideZonelessChangeDetection()],
+        });
+
+        const harnessLoader = TestbedHarnessEnvironment.loader(TestBed.createComponent(TestComponent));
+
+        return {
+            harness: await harnessLoader.getHarness(RootHarness),
+        };
+    }
+
+    it('should render', async () => {
+        const { harness } = await setupTest();
+        expect(harness).toBeDefined();
+    });
+});
