f676b25f5ac020251740f111fe9560f99af58b49
NoNamer777 <oscar.wellner@gmail.com>
2025-07-13 12:21:12 +0200

    Fix using protobuf files from project's own assets folder

--- libs/shared-api/src/lib/providers/user.provider.ts
+++ libs/shared-api/src/lib/providers/user.provider.ts
@@ -5,19 +5,19 @@ export const USER_PACKAGE_NAME = 'user';
 
 export const USER_CLIENT_NAME = 'USER_PACKAGE';
 
-export const USER_SERVICE_DEFINITION_PATH = 'assets/user.proto';
+export const USER_SERVICE_DEFINITION_PATH = 'assets/proto/user.proto';
 
 export const USER_SERVICE_HOST = '0.0.0.0:5000';
 
 export const USER_SERVICE_NAME = 'UsersService';
 
-export const withUserPackage = (basePath: string) => {
+export function withUserPackage(baseDir: string): ClientProviderOptions {
     return {
         name: USER_CLIENT_NAME,
         transport: Transport.GRPC,
         options: {
             package: USER_PACKAGE_NAME,
-            protoPath: join(basePath, USER_SERVICE_DEFINITION_PATH),
+            protoPath: join(baseDir, USER_SERVICE_DEFINITION_PATH),
         },
-    } satisfies ClientProviderOptions;
-};
+    };
+}
