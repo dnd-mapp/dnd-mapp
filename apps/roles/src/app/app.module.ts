17a94dd654155a9869e79381c690ff4ddb406476
NoNamer777 <oscar.wellner@gmail.com>
2025-07-24 22:40:41 +0200

    Add ScopesModule to make RPC ScopesService available

--- apps/roles/src/app/app.module.ts
+++ apps/roles/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { PrismaClient } from '../../prisma/client';
 import { AppController } from './app.controller';
 import { RolesModule } from './roles';
+import { ScopesModule } from './scopes';
 
 @Module({
-    imports: [DatabaseModule.forRoot(PrismaClient), RolesModule],
+    imports: [DatabaseModule.forRoot(PrismaClient), RolesModule, ScopesModule],
     controllers: [AppController],
 })
 export class AppModule {}
