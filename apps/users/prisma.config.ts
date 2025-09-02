1a308add3958360b543205c771a3001633bfa25d
Oscar Wellner <oscar.wellner@gmail.com>
2025-09-02 20:40:47 +0200

    Remove earlyAccess

--- apps/users/prisma.config.ts
+++ apps/users/prisma.config.ts
@@ -5,6 +5,5 @@ import { defineConfig } from 'prisma/config';
 config({ path: [join(__dirname, '../../.env'), join(__dirname, '.env')] });
 
 export default defineConfig({
-    earlyAccess: true,
     schema: join(__dirname, 'prisma', 'schema.prisma'),
 });
