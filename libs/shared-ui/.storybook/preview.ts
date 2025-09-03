513fb1dd9ef8c8e91af38baf0edc22af8f2a88f6
NoNamer777 <oscar.wellner@gmail.com>
2025-09-03 10:17:46 +0200

    Enable Angular animations in Storybook

--- libs/shared-ui/.storybook/preview.ts
+++ libs/shared-ui/.storybook/preview.ts
@@ -1,11 +1,17 @@
-import { componentWrapperDecorator, Preview } from '@storybook/angular';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { applicationConfig, componentWrapperDecorator, Preview } from '@storybook/angular';
 
 function wrapStory(story: string) {
     return `<main><section>${story}</section></main>`;
 }
 
 const preview: Preview = {
-    decorators: [componentWrapperDecorator((story) => wrapStory(story))],
+    decorators: [
+        componentWrapperDecorator((story) => wrapStory(story)),
+        applicationConfig({
+            providers: [provideAnimationsAsync()],
+        }),
+    ],
 };
 
 export default preview;
