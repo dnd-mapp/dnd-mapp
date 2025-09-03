6b472eaa25f032fd24750e2bf63f6fc743d588f6
NoNamer777 <oscar.wellner@gmail.com>
2025-09-03 10:16:43 +0200

    Update default story examples of simple components

--- libs/shared-ui/src/stories/checkbox/checkbox-story.component.ts
+++ libs/shared-ui/src/stories/checkbox/checkbox-story.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CheckboxComponent } from '@dnd-mapp/shared-ui';
 
 @Component({
     selector: 'dma-story',
     templateUrl: './checkbox-story.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
+    imports: [CheckboxComponent],
 })
 export class CheckboxStoryComponent {}
--- libs/shared-ui/src/stories/input/input-story.component.ts
+++ libs/shared-ui/src/stories/input/input-story.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputComponent } from '@dnd-mapp/shared-ui';
 
 @Component({
     selector: 'dma-story',
     templateUrl: './input-story.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
+    imports: [InputComponent],
 })
 export class InputStoryComponent {}
