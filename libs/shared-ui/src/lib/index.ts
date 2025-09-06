dbbce2f773bb3acb0910bb9f717547e14a5a3e40
NoNamer777 <oscar.wellner@gmail.com>
2025-09-06 13:56:00 +0200

    Remove navigation component

--- libs/shared-ui/src/lib/index.ts
+++ libs/shared-ui/src/lib/index.ts
@@ -3,7 +3,6 @@ export * from './checkbox';
 export * from './dialog';
 export * from './icons';
 export * from './input';
-export * from './navigation';
 export * from './side-bar';
 export * from './table';
 export * from './theming';
--- libs/shared-ui/src/lib/navigation/navigation.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'dma-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrl: './navigation.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class NavigationComponent {}
