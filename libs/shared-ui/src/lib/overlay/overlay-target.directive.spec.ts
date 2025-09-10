b54c13b7e0b1c4d01eda1eea765ab4aff161fa82
NoNamer777 <oscar.wellner@gmail.com>
2025-09-10 17:11:52 +0200

    Add OverlayTargetDirective

---
+++ libs/shared-ui/src/lib/overlay/overlay-target.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OverlayTargetDirective } from './overlay-target.directive';
+
+describe('OverlayTargetDirective', () => {
+    it('should create an instance', () => {
+        const directive = new OverlayTargetDirective();
+        expect(directive).toBeTruthy();
+    });
+});
