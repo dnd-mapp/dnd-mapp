a8a9886521b968692077bd791a4d4fc0126867d0
NoNamer777 <oscar.wellner@gmail.com>
2025-09-27 23:46:23 +0200

    Add CloseSideSheetButtonDirective

--- apps/web-client/src/app/shared/side-sheet/index.ts
+++ apps/web-client/src/app/shared/side-sheet/index.ts
@@ -1,3 +1,4 @@
+export * from './close-side-sheet-button';
 export * from './side-sheet.component';
 export * from './side-sheet.service';
 export * from './toggle-side-sheet-button';
--- apps/web-client/test/harnesses/shared/side-sheet/index.ts
+++ apps/web-client/test/harnesses/shared/side-sheet/index.ts
@@ -1,2 +1,3 @@
+export * from './close-side-sheet-button.harness';
 export * from './side-sheet.harness';
 export * from './toggle-side-sheet-button.harness';
