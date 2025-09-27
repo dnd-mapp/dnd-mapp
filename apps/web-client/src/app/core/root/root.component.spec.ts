8a6036a753c68f66abbe5cc0b7dd6c4bfad52747
NoNamer777 <oscar.wellner@gmail.com>
2025-09-27 23:40:32 +0200

    Add util function to setup test environment

--- apps/web-client/src/app/core/root/root.component.spec.ts
+++ apps/web-client/src/app/core/root/root.component.spec.ts
@@ -1,7 +1,5 @@
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { Component, provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import { RootHarness } from '@dnd-mapp/web-client/test';
+import { Component } from '@angular/core';
+import { RootHarness, setupEnvironment } from '@dnd-mapp/web-client/test';
 import { RootComponent } from './root.component';
 
 describe('RootComponent', () => {
@@ -12,15 +10,13 @@ describe('RootComponent', () => {
     class TestComponent {}
 
     async function setupTest() {
-        TestBed.configureTestingModule({
-            imports: [TestComponent],
-            providers: [provideZonelessChangeDetection()],
+        const { harness } = await setupEnvironment({
+            component: TestComponent,
+            harness: RootHarness,
         });
 
-        const harnessLoader = TestbedHarnessEnvironment.loader(TestBed.createComponent(TestComponent));
-
         return {
-            harness: await harnessLoader.getHarness(RootHarness),
+            harness: harness,
         };
     }
 
---
+++ apps/web-client/test/utils/setup-environment.ts
@@ -0,0 +1,23 @@
+import { ComponentHarness, HarnessQuery } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Type } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+interface SetupEnvironmentParams<Component, Harness extends ComponentHarness> {
+    component: Type<Component>;
+    harness: HarnessQuery<Harness>;
+}
+
+export async function setupEnvironment<Component, Harness extends ComponentHarness>(
+    params: SetupEnvironmentParams<Component, Harness>,
+) {
+    TestBed.configureTestingModule({
+        imports: [params.component],
+    });
+
+    const harnessLoader = TestbedHarnessEnvironment.loader(TestBed.createComponent(params.component));
+
+    return {
+        harness: await harnessLoader.getHarness(params.harness),
+    };
+}
