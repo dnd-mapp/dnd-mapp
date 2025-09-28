8722cfbd28a5675fafd804aa8a43ca73dda2a81d
NoNamer777 <oscar.wellner@gmail.com>
2025-09-28 12:14:53 +0200

    Expand SideSheetComponent to host component

--- apps/web-client/src/app/shared/side-sheet/side-sheet.component.ts
+++ apps/web-client/src/app/shared/side-sheet/side-sheet.component.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CdkPortalOutlet, ComponentPortal } from '@angular/cdk/portal';
+import { ChangeDetectionStrategy, Component, computed, input, Type } from '@angular/core';
+import { ThemeDirective } from '../theming';
 
 @Component({
     selector: 'dma-side-sheet',
     templateUrl: './side-sheet.component.html',
     styleUrl: './side-sheet.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
+    hostDirectives: [ThemeDirective],
+    imports: [CdkPortalOutlet],
 })
-export class SideSheetComponent {}
+export class SideSheetComponent<T> {
+    public readonly component = input<Type<T>>(null);
+
+    protected readonly componentPortal = computed(() => new ComponentPortal(this.component()));
+}
