e6debce37b13cf27befede3cf4834ce8aea94af0
NoNamer777 <oscar.wellner@gmail.com>
2025-09-29 15:13:59 +0200

    Add content to NavSidePanelComponent

--- apps/web-client/src/app/core/nav-side-panel/nav-side-panel.component.spec.ts
+++ apps/web-client/src/app/core/nav-side-panel/nav-side-panel.component.spec.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { NavSidePanelHarness, setupEnvironment } from '@dnd-mapp/web-client/test';
 import { NavSidePanelComponent } from './nav-side-panel.component';
 
@@ -13,6 +14,7 @@ describe('NavSidePanelComponent', () => {
         const { harness } = await setupEnvironment({
             component: TestComponent,
             harness: NavSidePanelHarness,
+            providers: [provideRouter([])],
         });
 
         return {
--- apps/web-client/src/app/core/nav-side-panel/nav-side-panel.component.ts
+++ apps/web-client/src/app/core/nav-side-panel/nav-side-panel.component.ts
@@ -1,10 +1,77 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import {
+    ArrowRightFromBracketSoIconComponent,
+    ArrowRightToBracketSoIconComponent,
+    BookSoIconComponent,
+    ButtonComponent,
+    CircleUserSoIconComponent,
+    CompassSoIconComponent,
+    HouseSoIconComponent,
+    NavLinkComponent,
+    NavMenuContainerComponent,
+    NavMenuTriggerComponent,
+    SideSheetService,
+    UserPlusSoIconComponent,
+    UsersSoIconComponent,
+} from '../../shared';
+import { User } from '../../user';
 
 @Component({
     selector: 'dma-nav-side-panel',
-    imports: [],
     templateUrl: './nav-side-panel.component.html',
     styleUrl: './nav-side-panel.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        NavLinkComponent,
+        HouseSoIconComponent,
+        UsersSoIconComponent,
+        CompassSoIconComponent,
+        BookSoIconComponent,
+        NavMenuTriggerComponent,
+        NavMenuContainerComponent,
+        ButtonComponent,
+        UserPlusSoIconComponent,
+        ArrowRightToBracketSoIconComponent,
+        CircleUserSoIconComponent,
+        ArrowRightFromBracketSoIconComponent,
+    ],
 })
-export class NavSidePanelComponent {}
+export class NavSidePanelComponent implements OnInit {
+    private readonly router = inject(Router);
+    private readonly destroyRef = inject(DestroyRef);
+    private readonly sideSheetService = inject(SideSheetService);
+
+    protected readonly authenticatedUser = signal<User>(null);
+
+    protected readonly isAuthenticated = computed(() => this.authenticatedUser() !== null);
+
+    public ngOnInit() {
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                takeUntilDestroyed(this.destroyRef),
+            )
+            .subscribe({
+                next: () => this.sideSheetService.toggle(),
+            });
+    }
+
+    protected onMyProfile() {
+        console.warn('NAVIGATING TO MY PROFILE PAGE');
+    }
+
+    protected onLogOut() {
+        console.warn('NAVIGATING TO MY LOG OUT PAGE');
+    }
+
+    protected onSignUp() {
+        console.warn('NAVIGATING TO SIGN UP PAGE');
+    }
+
+    protected onLogIn() {
+        console.warn('NAVIGATING TO LOG IN PAGE');
+    }
+}
