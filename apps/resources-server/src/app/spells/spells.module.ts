30992097ad2d9801b86ef92015ace891780e54ef
NoNamer777 <oscar.wellner@gmail.com>
2025-09-30 19:57:50 +0200

    Retrieve Spells from the database

--- apps/resources-server/src/app/spells/spells.module.ts
+++ apps/resources-server/src/app/spells/spells.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../shared';
 import { SpellsController } from './spells.controller';
 import { SpellsRepository } from './spells.repository';
 import { SpellsService } from './spells.service';
 
 @Module({
-    imports: [],
+    imports: [DatabaseModule],
     controllers: [SpellsController],
     providers: [SpellsRepository, SpellsService],
     exports: [SpellsService],
--- apps/resources-server/src/app/spells/spells.repository.ts
+++ apps/resources-server/src/app/spells/spells.repository.ts
@@ -1,9 +1,20 @@
-import { Spell } from '@dnd-mapp/shared';
+import { Spell, transformAll } from '@dnd-mapp/shared';
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../shared';
 
 @Injectable()
 export class SpellsRepository {
+    private readonly databaseService: DatabaseService;
+
+    constructor(databaseService: DatabaseService) {
+        this.databaseService = databaseService;
+    }
+
     public async findAll() {
-        return Promise.resolve<Spell[]>([]);
+        const results = await this.databaseService.spells.findMany({
+            orderBy: { name: 'asc' },
+        });
+
+        return transformAll(results, Spell);
     }
 }
