7f3c249320345535231dfced358aa371028119d4
NoNamer777 <oscar.wellner@gmail.com>
2025-10-02 13:45:51 +0200

    Add theming and styling to resources-client

--- apps/resources-client/src/app/core/root/root.component.ts
+++ apps/resources-client/src/app/core/root/root.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ThemeDirective } from '@dnd-mapp/shared-ui';
 
 @Component({
     selector: 'dma-root',
     templateUrl: './root.component.html',
     styleUrl: './root.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    hostDirectives: [ThemeDirective],
     imports: [],
 })
 export class RootComponent {}
