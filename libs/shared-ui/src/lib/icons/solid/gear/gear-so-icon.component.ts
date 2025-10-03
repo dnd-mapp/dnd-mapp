f24edbdb38dccdf303d6571b940ffc8b367ff157
NoNamer777 <oscar.wellner@gmail.com>
2025-10-03 10:55:18 +0200

    Add Icons

---
+++ libs/shared-ui/src/lib/icons/solid/gear/gear-so-icon.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-gear-so-icon]',
+    templateUrl: './gear-so-icon.component.svg',
+    styleUrls: ['../../icons.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GearSoIconComponent {}
