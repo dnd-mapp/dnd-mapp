1ea3466d6de23ea1c5567a02aea97285959495df
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-09 17:19:09 +0100

    Chore: Move app config files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/core/root/root.component';
+import { appConfig } from './app/core/config/app.config';
+import { RootComponent } from './app/core/root/root.component';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(RootComponent, appConfig).catch((err) => console.error(err));
