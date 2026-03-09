251bea8106308fb4d0ff2a75929b2adb4cbe07c9
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-09 16:05:32 +0100

    Chore: Run prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'dma-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'dma-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class App {
-  protected readonly title = signal('dnd-mapp');
+    protected readonly title = signal('dnd-mapp');
 }
