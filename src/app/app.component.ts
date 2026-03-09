1d85641565f2575730c54789f5468f765b507434
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-09 17:13:52 +0100

    Chore: Remove default generated application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'dma-root',
-    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
+    imports: [RouterOutlet],
 })
-export class App {
-    protected readonly title = signal('dnd-mapp');
-}
+export class App {}
