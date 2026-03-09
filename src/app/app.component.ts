4bcf72a63b9e1ca2e540daa53255e4160778ed74
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-09 17:14:17 +0100

    Feat: Enable OnPush change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'dma-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [RouterOutlet],
 })
 export class App {}
