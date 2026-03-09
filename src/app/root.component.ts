99765ea413536995722b1bdec34efc45f79cd455
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-09 17:18:10 +0100

    Chore: Rename AppComponent

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'dma-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+    templateUrl: './root.component.html',
+    styleUrl: './root.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [RouterOutlet],
 })
-export class App {}
+export class RootComponent {}
