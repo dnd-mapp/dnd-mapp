7e1592c70a3b0b66b546a501567fab9e11ff1a4a
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-11 16:54:38 +0100

    Feat: Initialize translations upon application start

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -1,8 +1,14 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTranslations } from '../../shared/ui';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient(withFetch())],
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes),
+        provideHttpClient(withFetch()),
+        provideTranslations(),
+    ],
 };
