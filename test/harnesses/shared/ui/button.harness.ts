a3b7bae30cf6d5749df955f6481517159e4a92ef
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-11 23:37:12 +0100

    Feat: Add icon only buttons

--- test/harnesses/shared/ui/button.harness.ts
+++ test/harnesses/shared/ui/button.harness.ts
@@ -3,6 +3,14 @@ import { ComponentHarness } from '@angular/cdk/testing';
 export class ButtonHarness extends ComponentHarness {
     public static readonly hostSelector = 'button[dma-button]';
 
+    public async isIconOnly() {
+        return await (await this.host()).hasClass('icon-only');
+    }
+
+    public async label() {
+        return await (await this.host()).text();
+    }
+
     public async color() {
         return await (await this.host()).getAttribute('dma-button');
     }
