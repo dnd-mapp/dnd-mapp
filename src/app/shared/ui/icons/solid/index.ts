fbae1d506fd0d580e96f08dd251e6108bfbaccb7
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-12 10:11:29 +0100

    Feat: Add BookOpen-, Dungeon-, and UsersIcon

--- src/app/shared/ui/icons/solid/index.ts
+++ src/app/shared/ui/icons/solid/index.ts
@@ -1,4 +1,7 @@
 export * from './angle-left/angle-left.icon';
 export * from './angle-right/angle-right.icon';
 export * from './arrow-right-to-bracket/arrow-right-to-bracket.icon';
+export * from './book-open/book-open.icon';
+export * from './dungeon/dungeon.icon';
 export * from './user-plus/user-plus.icon';
+export * from './users/users.icon';
