1a790da25b91ea1ffc2e81ffbff8e13ea706d9ee
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-14 11:11:24 +0100

    Feat: Add translation keys

--- src/common/localization/translations.ts
+++ src/common/localization/translations.ts
@@ -3,6 +3,10 @@ export interface Translations {
     BTN_LABEL_SIDE_PANEL_CLOSE: string;
     BTN_LABEL_SIDE_PANEL_OPEN: string;
     BTN_LABEL_SIGN_UP: string;
+    HOME_BTN_LABEL_GET_STARTED: string;
+    HOME_BTN_LABEL_NAV_COMPENDIUM: string;
+    HOME_HERO_SUBTITLE: string;
+    HOME_HERO_DESCRIPTION: string;
     LABEL_CAMPAIGNS: string;
     LABEL_CHARACTERS: string;
     LABEL_COMPENDIUM: string;
